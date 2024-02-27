1a04b798afdf2a085f99aaa3bbe050afa242f654
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-02-27 22:02:46 +0200

    addinf functions

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-import {updateGround} from "./ground"
+import { setupGround, updateGround } from "./ground";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
 
 const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
 
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener("keydown", handleStart, { once: true });
+
+setupGround();
 
 let lastTime;
+let speedScale;
+let score;
 function update(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -17,12 +24,32 @@ function update(time) {
     return;
   }
   const delta = time - lastTime;
-  lastTime = time;
-  updateGround(delta);
 
+  updateGround(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
+
+  lastTime = time;
   window.requestAnimationFrame(update);
 }
 window.requestAnimationFrame(update);
+
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_SCALE_INCREASE;
+}
+
+function updateScore(delta) {
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score);
+}
+
+function handleStart() {
+  lastTime = null;
+  speedScale = 1;
+  score = 0;
+  setupGround();
+  window.requestAnimationFrame(update);
+}
 
 function setPixelToWorldScale() {
   let worldToPixelScale;
